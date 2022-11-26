228bb64ff2be6dbf292a38a7021b8fa436f29295
sb <88203775+Sbtech24@users.noreply.github.com>
2022-11-26 14:22:47 +0100

    advice generator

---
+++ script.js
@@ -0,0 +1,28 @@
+
+function Refresh(){
+    fetchData()
+}
+
+function fetchData(){
+   fetch('https://api.adviceslip.com/advice')
+   .then((response)=>response.json())
+   .then((data)=>{
+
+    // console.log(data)
+    let quoteId = data.slip.id
+    let adviceId = document.getElementById('adviceId')
+    adviceId.textContent="ADVICE #" +" "+quoteId
+
+    let quote = data.slip.advice;
+    let adviceEl = document.getElementById('advice')
+    // console.log(data.slip.advice)
+    adviceEl.textContent = quote;
+
+
+    
+   })
+
+}   
+
+
+setInterval(fetchQuote,1);
